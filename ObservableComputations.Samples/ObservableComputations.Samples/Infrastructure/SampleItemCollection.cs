0683e6ef1cebb444797362a25e5f97125e0ad4ac
Igor Buchelnikov <igor_buchelnikov_github@mail.ru>
2020-04-16 13:59:42 +0300

    work

--- ObservableComputations.Samples/ObservableComputations.Samples/Infrastructure/SampleItemCollection.cs
+++ ObservableComputations.Samples/ObservableComputations.Samples/Infrastructure/SampleItemCollection.cs
@@ -15,28 +15,28 @@ namespace ObservableComputations.Samples.Infrastructure
                 new SampleItem("Selectable Items", new SelectableItemsViewModel(),
                         "Items order in the result collections is the original collection order",
                         "SelectableItemsViewModel.cs",
-                        @"https://github.com/RolandPheasant/DynamicData.Samplz/blob/master/DynamicData.Samplz/Examples/SelectableItemsViewModel.cs",
+                        @"https://github.com/IgorBuchelnikov/ObservableComputations.Samples/blob/master/DynamicData.Samplz/Examples/SelectableItemsViewModel.cs",
                         "SelectableItemsViewModel.cs",
                         @"https://github.com/IgorBuchelnikov/ObservableComputations.Samples/blob/master/ObservableComputations.Samples/Examples/SelectableItemsViewModel.cs"),
 
                 new SampleItem("Aggregations", new AggregationViewModel(),
                         "Aggregate over a collection which is filtered on a property",
                         "AggregationViewModel.cs",
-                        @"https://github.com/RolandPheasant/DynamicData.Samplz/blob/master/DynamicData.Samplz/Examples/AggregationViewModel.cs",
+                        @"https://github.com/IgorBuchelnikov/ObservableComputations.Samples/blob/master/DynamicData.Samplz/Examples/AggregationViewModel.cs",
                         "AggregationViewModel.cs",
                         @"https://github.com/IgorBuchelnikov/ObservableComputations.Samples/blob/master/ObservableComputations.Samples/Examples/AggregationViewModel.cs"),
 
                new SampleItem("Add and remove items", new  AddRemoveItemsViewModel(), 
                         "Items order in the result collections is the asceding order of time of the last add action",
                         "FilterObservableViewModel.cs",
-                        @"https://github.com/RolandPheasant/DynamicData.Samplz/blob/master/DynamicData.Samplz/Examples/FilterObservableViewModel.cs",
+                        @"https://github.com/IgorBuchelnikov/ObservableComputations.Samples/blob/master/DynamicData.Samplz/Examples/FilterObservableViewModel.cs",
                         "AddRemoveItemsViewModel.cs",
                         @"https://github.com/IgorBuchelnikov/ObservableComputations.Samples/blob/master/ObservableComputations.Samples/Examples/AddRemoveItemsViewModel.cs"),
 
                 new SampleItem("One to many join", new  ParentChildrenViewModel(), 
                         "Managing parent's children",
                         "JoinManyViewModel.cs",
-                        @"https://github.com/RolandPheasant/DynamicData.Samplz/blob/master/DynamicData.Samplz/Examples/JoinViewModel.cs",
+                        @"https://github.com/IgorBuchelnikov/ObservableComputations.Samples/blob/master/DynamicData.Samplz/Examples/JoinViewModel.cs",
 		                "ParentChildrenViewModel.cs",
 		                @"https://github.com/IgorBuchelnikov/ObservableComputations.Samples/blob/master/ObservableComputations.Samples/Examples/ParentChildrenViewModel.cs")
 
